111cf73674bf33bf284eff21088a2655e92b5bdb
kunalchhabra37 <kunalchharba37@gmail.com>
2022-11-24 13:24:31 +0530

    another one

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-const  {WebSocket, createWebSocketStream } = require("ws");
+const { WebSocket, createWebSocketStream } = require("ws");
 
-let socket = new WebSocket("ws://localhost:9001");
+// socket.onopen = function (e) {
+//   console.log("[open] Connection established");
+//   console.log("Sending to server");
+//   socket.send("My name is John");
+// };
 
-socket.onopen = function(e) {
-  console.log("[open] Connection established");
-  console.log("Sending to server");
-  socket.send("My name is John");
-};
-
-socket.onmessage = function(event) {
+socket.onmessage = function (event) {
   console.log(`[message] Data received from server: ${event.data}`);
 };
 
-socket.onclose = function(event) {
+socket.onclose = function (event) {
   if (event.wasClean) {
-    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    console.log(
+      `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`
+    );
   } else {
     // e.g. server process killed or network down
     // event.code is usually 1006 in this case
-    console.log('[close] Connection died');
+    console.log("[close] Connection died");
   }
 };
 
-socket.onerror = function(error) {
+socket.onerror = function (error) {
   console.log(`[error]`);
 };
+
+const create = async () => {
+  let socket = new WebSocket("ws://localhost:9001");
+
+  console.log("establishing connection...");
+  socket.send({
+    jsonrpc: "2.0",
+    id: 1,
+    method: "sui_subscribeEvent",
+    params: [
+      { All: [{ Package: "0x26d943059b2c6420671bc7c66b3e72b8532f0e7f" }] },
+    ],
+  });
+};
+
+
