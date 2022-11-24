b1ab163ee0b8cd4919d1c3c98687855ff34808d7
kunalchhabra37 <kunalchharba37@gmail.com>
2022-11-24 14:53:19 +0530

    another one

--- index2.js
+++ index2.js
@@ -34,10 +34,13 @@ let socket = new WebSocket("ws://localhost:9001");
 // 
 
 
-const main = () => {
+const main = async () => {
     socket.on("open", () => {
         console.log("connected to ws");
     });
+
+    let res = socket.send({"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"Package":"0x26d943059b2c6420671bc7c66b3e72b8532f0e7f"}]}]});
+    console.log(res);
 }
 
 main()
