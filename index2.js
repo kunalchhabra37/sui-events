29092a34a2b77fa0cf2e1ee330341b81fda59646
kunalchhabra37 <kunalchharba37@gmail.com>
2022-11-24 16:37:06 +0530

    another one

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
 const  {WebSocket, createWebSocketStream } = require("ws");
 const { JSONRPCClient } = require("json-rpc-2.0");
-// let socket = new WebSocket("ws://localhost:9001");
+let socket = new WebSocket("ws://localhost:9001");
 
 
 // socket.onopen = function(e) {
@@ -27,70 +27,89 @@ const { JSONRPCClient } = require("json-rpc-2.0");
 //   console.log(`[error]`);
 // };
 
-// const main = async () => {
-
-//     console.log("establishing connection...");
-//     socket.send({"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"Package":"0x26d943059b2c6420671bc7c66b3e72b8532f0e7f"}]}]});
-
-// 
 
 
-// const main = async () => {
-//     socket.on("open", async () => {
-//         console.log("connected to ws");
-//         let res = await socket.emit('rpc', {"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"Package":"0x26d943059b2c6420671bc7c66b3e72b8532f0e7f"}]}]});
-//         console.log(res);
-
-//         socket.on("message", (res) => {
-//             console.log(`res; ${res}`);
-//         })
-//     });
 
 
+const main = async () => {
+    socket.on("open", async () => {
+        console.log("connected to ws");
+        let rpcObject = {"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"Package":"0x26d943059b2c6420671bc7c66b3e72b8532f0e7f"}]}]}
+        // let res = await socket.emit('rpc', {"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"Package":"0x26d943059b2c6420671bc7c66b3e72b8532f0e7f"}]}]});
+        let res = await res.send(JSON.stringify(rpcObject));
+        console.log(`response rpc: ${res}`);
+
+        socket.on("message", (res) => {
+            console.log(`res; ${res}`);
+        })
+    });
+}
+
+main()
+
+// var JSONRPC_TIMEOUT_MS = 1000;
+
+// var jsonrpc = async function(url, onopen, onclose, onnotification) {
+//   var rpcid = 0, pending = {}, ws = new WebSocket(url);
+//   if (!ws) return null;
+//   ws.onclose = onclose;
+//   ws.onmessage = function(ev) {
+//     const frame = JSON.parse(ev.data);
+//     console.log('rcvd', frame, 'pending:', pending);
+//     if (frame.id !== undefined) {
+//       if (pending[frame.id] !== undefined) pending[frame.id](frame);  // Resolve
+//       delete (pending[frame.id]);
+//     } else {
+//       if (onnotification) onnotification(frame);
+//     }
+//   };
+//   if (onopen) {
+//     onopen();
+//     let request = {"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"Package":"0x26d943059b2c6420671bc7c66b3e72b8532f0e7f"}]}]}
+//     ws.send(JSON.stringify(request));
+//     console.log('sent', request);
+//     let res = await resolveRequest(ws);
+//     console.log(`respons rpc: ${res}`);
+// };
+// //   return {
+// //     close: () => ws.close(),
+// //     call: function(method, params) {
+// //       const id = rpcid++, request = {id, method, params};
+// //       ws.send(JSON.stringify(request));
+// //       console.log('sent', request);
+// //       return new Promise(function(resolve, reject) {
+// //         setTimeout(JSONRPC_TIMEOUT_MS, function() {
+// //           if (pending[id] === undefined) return;
+// //           log('Timing out frame ', JSON.stringify(request));
+// //           delete (pending[id]);
+// //           reject();
+// //         });
+// //         pending[id] = x => resolve(x);
+// //       });
+// //     },
+// //   };
+// };
+
+// jsonrpc("ws://localhost:9001", () => {
+//     console.log("connected")
+
+// }, () => {
+//     console.log("disconnected");
+// }, (data) => {
+//     console.log(`data: ${data}`);
+// });
+
+
+// let resolveRequest = (ws) => {
+//     let id = 1;
+//     let pending = {};
+//     return new Promise(function(resolve, reject) {
+//         setTimeout(JSONRPC_TIMEOUT_MS, function() {
+//           if (pending[id] === undefined) return;
+//           log('Timing out frame ', JSON.stringify(request));
+//           delete (pending[id]);
+//           reject();
+//         });
+//         pending[id] = x => resolve(x);
+//       });
 // }
-
-// main()
-
-var JSONRPC_TIMEOUT_MS = 1000;
-
-var jsonrpc = function(url, onopen, onclose, onnotification) {
-  var rpcid = 0, pending = {}, ws = new WebSocket(url);
-  if (!ws) return null;
-  ws.onclose = onclose;
-  ws.onmessage = function(ev) {
-    const frame = JSON.parse(ev.data);
-    console.log('rcvd', frame, 'pending:', pending);
-    if (frame.id !== undefined) {
-      if (pending[frame.id] !== undefined) pending[frame.id](frame);  // Resolve
-      delete (pending[frame.id]);
-    } else {
-      if (onnotification) onnotification(frame);
-    }
-  };
-  if (onopen) onopen();
-  return {
-    close: () => ws.close(),
-    call: function(method, params) {
-      const id = rpcid++, request = {id, method, params};
-      ws.send(JSON.stringify(request));
-      console.log('sent', request);
-      return new Promise(function(resolve, reject) {
-        setTimeout(JSONRPC_TIMEOUT_MS, function() {
-          if (pending[id] === undefined) return;
-          log('Timing out frame ', JSON.stringify(request));
-          delete (pending[id]);
-          reject();
-        });
-        pending[id] = x => resolve(x);
-      });
-    },
-  };
-};
-
-jsonrpc("ws://localhost:9001", () => {
-    console.log("connected")
-}, () => {
-    console.log("disconnected");
-}, (data) => {
-    console.log(`data: ${data}`);
-});
