62c6c30faa7a327c81bc5392cb469d4420903b1d
kunalchhabra37 <kunalchharba37@gmail.com>
2022-11-24 15:33:53 +0530

    another one

--- index2.js
+++ index2.js
@@ -1,4 +1,5 @@
 const  {WebSocket, createWebSocketStream } = require("ws");
+const { JSONRPCClient } = require("json-rpc-2.0");
 let socket = new WebSocket("ws://localhost:9001");
 
 
@@ -37,7 +38,8 @@ let socket = new WebSocket("ws://localhost:9001");
 const main = async () => {
     socket.on("open", async () => {
         console.log("connected to ws");
-        let res = await socket.send('{"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"Package":"0x26d943059b2c6420671bc7c66b3e72b8532f0e7f"}]}]}');
+        let res = await socket.emit('rpc', {"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"Package":"0x26d943059b2c6420671bc7c66b3e72b8532f0e7f"}]}]});
+        
         console.log(res);
     });
 
