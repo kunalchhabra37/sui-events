bc797c26c29e54489635adade6f0b6981725e821
kunalchhabra37 <kunalchharba37@gmail.com>
2022-11-24 15:41:38 +0530

    another one

--- index2.js
+++ index2.js
@@ -39,8 +39,11 @@ const main = async () => {
     socket.on("open", async () => {
         console.log("connected to ws");
         let res = await socket.emit('rpc', {"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"Package":"0x26d943059b2c6420671bc7c66b3e72b8532f0e7f"}]}]});
-        
         console.log(res);
+
+        socket.on("message", (res) => {
+            console.log(`res; ${res}`);
+        })
     });
 
 }
