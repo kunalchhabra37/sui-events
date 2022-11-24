bb115e7dc7a577da159e15d67713f8d978fbffa4
kunalchhabra37 <kunalchharba37@gmail.com>
2022-11-24 16:39:48 +0530

    another one

--- index2.js
+++ index2.js
@@ -36,8 +36,8 @@ const main = async () => {
         console.log("connected to ws");
         let rpcObject = {"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"Package":"0x26d943059b2c6420671bc7c66b3e72b8532f0e7f"}]}]}
         // let res = await socket.emit('rpc', {"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"Package":"0x26d943059b2c6420671bc7c66b3e72b8532f0e7f"}]}]});
-        let res = await res.send(JSON.stringify(rpcObject));
-        console.log(`response rpc: ${res}`);
+        let result = await res.send(JSON.stringify(rpcObject));
+        console.log(`response rpc: ${result}`);
 
         socket.on("message", (res) => {
             console.log(`res; ${res}`);
